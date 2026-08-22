f4a903c065600fba8cfb1d552bb4b64e7a9ac5dd
nishant mishra <nish57477@gmail.com>
2026-08-22 20:10:30 +0530

    added calculator

---
+++ calculator.py
@@ -0,0 +1,7 @@
+a = float(input("enter first number:"))
+b = float(input("enter second number:"))
+
+print("addition:",a + b)
+print("subtraction:",a - b)
+print("multiply:",a * b)
+print("division:",a / b)
